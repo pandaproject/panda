51beaa792d157bb16b4cd103c9506942dc318c18
Christopher Groskopf <staringmonkey@gmail.com>
2011-09-28 11:10:43 -0500

    Add jquery and a proper base template.

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+
+from django.conf import settings
 from django.conf.urls.defaults import include, patterns
 from django.contrib import admin
 
@@ -6,4 +9,9 @@ admin.autodiscover()
 urlpatterns = patterns('',
     (r'', include('redd.urls')),
     (r'^admin/', include(admin.site.urls)),
+
+    # Should never be used in production, as nginx will server these paths
+    (r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
+        { 'document_root': settings.MEDIA_ROOT,
+            'show_indexes': True }),
 )
