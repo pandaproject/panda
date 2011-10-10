158e8ed1ee7b39423790f11d62b869d777e6bf30
Christopher Groskopf <staringmonkey@gmail.com>
2011-10-10 18:22:24 -0500

    Backbonified the uploader.

---
+++ forest/static/js/views/upload.js
@@ -0,0 +1,62 @@
+Redd.views.Upload = Backbone.View.extend({
+    el: $("#content"),
+    
+    template: JST.upload,
+
+    file_uploader: null,
+    current_dataset: null,
+
+    events: {
+        "submit #dataset-form": "create_dataset",
+        "submit #import-form": "import_data"
+    },
+
+    initialize: function() {
+        _.bindAll(this, "render", "create_dataset", "import_data");
+        
+        this.render();
+
+        this.file_uploader = new qq.FileUploader({
+            action: "/ajax_upload/",
+            element: $("#upload")[0],
+            multiple: false,
+            onComplete: function(id, fileName, responseJSON) {
+                if(responseJSON.success) {
+                    $("#dataset-data-upload").val(responseJSON["id"]);
+                    $("#dataset").show();
+                } else {
+                    alert("Upload failed!");
+                }
+            }
+        });
+    },
+
+    render: function() {
+        this.el.html(this.template());
+    },
+
+    create_dataset: function() {
+        this.current_dataset = new Redd.models.Dataset({
+            name: $("#dataset-form #dataset-name").val(),
+            data_upload: { id: $("#dataset-form #dataset-data-upload").val() }
+        });
+        this.current_dataset.save();
+
+        $("#import-form #import-id").val(this.current_dataset.get("id"));
+        $("#import").show();
+
+        return false;
+    },
+
+    import_data: function() {
+        this.current_dataset.import_data(
+            function(d) {
+                alert(d.get("current_task_id"));
+            }
+        );
+
+        return false;
+    }
+});
+
+
--- forest/views.py
+++ forest/views.py
@@ -8,23 +8,6 @@ from django.http import HttpResponse
 from django.middleware.csrf import get_token
 from django.shortcuts import render_to_response
 
-def search(request):
-    """
-    Simples search UI.
-    """
-    return render_to_response('search.html', {
-        'STATIC_URL': settings.STATIC_URL
-        }) 
-
-def upload(request):
-    """
-    UI for uploading a file.
-    """
-    return render_to_response('upload.html', {
-        'STATIC_URL': settings.STATIC_URL,
-        'csrf_token': get_token(request)
-        })
-
 def index(request):
     """
     Page shell for the client-side application.
@@ -32,7 +15,7 @@ def index(request):
     return render_to_response('index.html', {
         'STATIC_URL': settings.STATIC_URL,
         'csrf_token': get_token(request)
-        })
+    })
 
 def jst(request):
     """
@@ -44,17 +27,22 @@ def jst(request):
 
     for dirpath, dirnames, filenames in os.walk(templates_path):
         for filename in filenames:
+            name, extension = os.path.splitext(filename)
+
+            if extension != '.jst':
+                continue
+
             fullpath = os.path.join(dirpath, filename)
 
             with open(fullpath, 'r') as f:
                 contents = f.read()
 
-            # From django-pipeline
+            # Borrowed from django-pipeline
             contents = re.sub(r"\r?\n", "", contents)
             contents = re.sub(r"'", "\\'", contents)
 
             compiled += "JST['%s'] = _.template('%s');\n" % (
-                os.path.splitext(filename)[0],
+                name,
                 contents
             )
 
