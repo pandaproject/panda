e81866c4c2a032eb14312132871fea7effd5183c
Christopher Groskopf <staringmonkey@gmail.com>
2011-10-11 17:23:21 -0500

    Implement upload file download endpoint.

--- redd/api.py
+++ redd/api.py
@@ -8,6 +8,8 @@ from celery.states import EXCEPTION_STATES
 from celery.task.control import inspect 
 from django.conf import settings
 from django.conf.urls.defaults import url
+from django.http import HttpResponse
+from django.core.servers.basehttp import FileWrapper
 from django.core.urlresolvers import reverse
 from sunburnt import SolrInterface
 from sunburnt.search import SolrSearch
@@ -166,6 +168,38 @@ class UploadResource(ModelResource):
         # TKTK
         authentication = Authentication()
         authorization = Authorization()
+
+    def override_urls(self):
+        """
+        Add urls for search endpoint.
+        """
+        return [
+            url(r'^(?P<resource_name>%s)/(?P<pk>\w[\w/-]*)/download%s$' % (self._meta.resource_name, trailing_slash()), self.wrap_view('download'), name='api_download_upload'),
+        ]
+
+    def download(self, request, **kwargs):
+        """
+        Download the original file that was uploaded.
+        """
+        self.method_check(request, allowed=['get'])
+        self.is_authenticated(request)
+        self.throttle_check(request)
+
+        if 'pk' in kwargs:
+            get_id = kwargs['pk']
+        else:
+            get_id = request.GET.get('id', '')
+
+        upload = Upload.objects.get(id=get_id)
+        path = upload.get_path()
+
+        self.log_throttled_access(request)
+
+        response = HttpResponse(FileWrapper(open(path, 'r')))
+        response['Content-Disposition'] = 'attachment; filename=%s' % upload.original_filename
+        response['Content-Length'] = upload.size
+
+        return response
 
 class DatasetResource(CustomResource):
     """
