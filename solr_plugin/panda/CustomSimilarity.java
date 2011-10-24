a3f376382fbd337442415b9304a39bc9c0a33bec
Christopher Groskopf <staringmonkey@gmail.com>
2011-10-24 11:19:33 -0500

    Tweak solr similarity to not compute tf. Closes #51.

--- solr_plugin/panda/CustomSimilarity.java
+++ solr_plugin/panda/CustomSimilarity.java
@@ -3,6 +3,9 @@ import org.apache.lucene.search.DefaultSimilarity;
 
 
 public class CustomSimilarity extends DefaultSimilarity {
+	public float tf(float freq) {
+		return 1;
+	}
 	public float idf(int i, int i1) {
 		return 1;
 	}
