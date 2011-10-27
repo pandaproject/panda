e2a2fc89565b22e2aa4c6141e06d963b8a8db65b
Christopher Groskopf <staringmonkey@gmail.com>
2011-10-27 12:52:16 -0500

    Make filename box bigger.

--- forest/static/js/views/edit_dataset.js
+++ forest/static/js/views/edit_dataset.js
@@ -25,7 +25,7 @@ PANDA.views.EditDataset = Backbone.View.extend({
 
         if (task && task.get("task_name") == "redd.tasks.DatasetImportTask") {
             if (task.get("status") == "STARTED") {
-                $("#edit-dataset-alert").alert("info block-message", "<p><strong>Import in progress!</strong> This dataset is currently being made searchable. It will not yet appear in search results.</p><strong>Status:</strong> " + task.get("message") + ".");
+                $("#edit-dataset-alert").alert("info block-message", "<p><strong>Import in progress!</strong> This dataset is currently being made searchable. It will not yet appear in search results.</p>Status of import: " + task.get("message") + ".");
             } else if (task.get("status") == "FAILURE") {
                 $("#edit-dataset-alert").alert("error block-message", "<p><strong>Import failed!</strong> The process to make this dataset searchable failed. It will not appear in search results.");
             } 
