9319c42701a9f1ba430cd6d3fb4965e551a490f5
Christopher Groskopf <staringmonkey@gmail.com>
2011-11-07 15:52:54 -0600

    Added api test for search.

--- redd/tests/test_api_data.py
+++ redd/tests/test_api_data.py
@@ -65,5 +65,42 @@ class TestAPIData(TestCase):
         self.assertEqual(response.status_code, 405)
 
     def test_search(self):
-        pass
+        self.dataset.import_data()
+
+        utils.wait()
+
+        # Import second dataset so we can make sure both match 
+        second_dataset = Dataset.objects.create(
+            name='Second dataset',
+            data_upload=self.dataset.data_upload)
+
+        second_dataset.import_data()
+
+        utils.wait()
+
+        response = self.client.get('/api/1.0/data/search/?q=Christopher')
+
+        self.assertEqual(response.status_code, 200)
+
+        body = json.loads(response.content)
+
+        # Verify that the group count is correct
+        self.assertEqual(body['meta']['total_count'], 2)
+        self.assertEqual(len(body['objects']), 2)
+
+        # Verify that each matched dataset includes one result
+        for result_dataset in body['objects']:
+            self.assertEqual(result_dataset['meta']['total_count'], 1)
+            self.assertEqual(len(result_dataset['objects']), 1)
+
+            db_dataset = Dataset.objects.get(id=result_dataset['id'])
+            
+            self.assertEqual(result_dataset['name'], db_dataset.name)
+            self.assertEqual(result_dataset['row_count'], db_dataset.row_count)
+            self.assertEqual(result_dataset['schema'], db_dataset.schema)
+
+            self.assertEqual(result_dataset['objects'][0]['data'][0], 'Christopher')
+            self.assertIn('id', result_dataset['objects'][0])
+            self.assertIn('resource_uri', result_dataset['objects'][0])
+            self.assertIn('row', result_dataset['objects'][0])
 
