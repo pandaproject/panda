a7aa9301daeff4c93bd907f51114af68b36669df
Christopher Groskopf <staringmonkey@gmail.com>
2011-11-07 15:26:45 -0600

    Add test_get for api data (and missing file).

---
+++ redd/tests/test_api_data.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+from django.conf import settings
+from django.test import TestCase
+from django.test.client import Client
+from django.utils import simplejson as json
+
+from redd.models import Dataset
+from redd.tests import utils
+
+class TestAPIData(TestCase):
+    def setUp(self):
+        settings.CELERY_ALWAYS_EAGER = True
+
+        self.solr = utils.get_test_solr() 
+
+        self.upload = utils.get_test_upload()
+        self.dataset = utils.get_test_dataset(self.upload)
+
+        self.client = Client()
+    
+    def test_get(self):
+        self.dataset.import_data()
+
+        utils.wait()
+
+        response = self.client.get('/api/1.0/data/')
+        self.assertEqual(response.status_code, 200)
+        body = json.loads(response.content)
+
+        list_result = body['objects'][0]
+
+        response = self.client.get('/api/1.0/data/%s/' % list_result['id'])
+        self.assertEqual(response.status_code, 200)
+        get_result = json.loads(response.content)
+
+        self.assertEqual(list_result, get_result)
+
+    def test_list(self):
+        self.dataset.import_data()
+
+        utils.wait()
+
+        response = self.client.get('/api/1.0/data/')
+
+        self.assertEqual(response.status_code, 200)
+
+        body = json.loads(response.content)
+
+        self.assertEqual(body['meta']['total_count'], 4)
+        self.assertEqual(body['objects'][0]['dataset'], '/api/1.0/dataset/%i/' % self.dataset.id)
+        self.assertIn('data', body['objects'][0])
+        self.assertIn('id', body['objects'][0])
+        self.assertIn('resource_uri', body['objects'][0])
+        self.assertIn('row', body['objects'][0])
+
+    def test_create(self):
+        pass
+
+    def test_search(self):
+        pass
+
