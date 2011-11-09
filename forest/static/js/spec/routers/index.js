0397b0c4740ffa8e47d0956d3b715692e2773823
Christopher Groskopf <staringmonkey@gmail.com>
2011-11-09 11:34:16 -0600

    Complete router tests.

--- forest/static/js/spec/routers/index.js
+++ forest/static/js/spec/routers/index.js
@@ -18,7 +18,7 @@ describe("Index Router", function() {
         var route_spy = sinon.spy();
         this.router.bind("route:search", route_spy);
 
-        this.controller_mock.expects("search").withArgs().once();
+        this.controller_mock.expects("search").withExactArgs(undefined, undefined, undefined).once();
 
         this.router.navigate("", true);
 
@@ -29,7 +29,7 @@ describe("Index Router", function() {
     });
 
     it("should support search query", function() {
-        this.controller_mock.expects("search").withArgs("query").once();
+        this.controller_mock.expects("search").withExactArgs("query", undefined, undefined).once();
 
         this.router.navigate("search/query", true);
 
@@ -37,7 +37,7 @@ describe("Index Router", function() {
     });
 
     it("should support search limit", function() {
-        this.controller_mock.expects("search").withArgs("query", "10").once();
+        this.controller_mock.expects("search").withExactArgs("query", "10", undefined).once();
 
         this.router.navigate("search/query/10", true);
 
@@ -45,9 +45,73 @@ describe("Index Router", function() {
     });
 
     it("should support search paging", function() {
-        this.controller_mock.expects("search").withArgs("query", "10", "2").once();
+        this.controller_mock.expects("search").withExactArgs("query", "10", "2").once();
 
         this.router.navigate("search/query/10/2", true);
+
+        this.controller_mock.verify();
+    });
+
+    it("should support upload", function() {
+        this.controller_mock.expects("upload").once();
+
+        this.router.navigate("upload", true);
+
+        this.controller_mock.verify();
+    });
+
+    it("should support dataset browsing", function() {
+        this.controller_mock.expects("list_datasets").withExactArgs(undefined, undefined).once();
+
+        this.router.navigate("datasets", true);
+
+        this.controller_mock.verify();
+    });
+
+    it("should support dataset browsing limit", function() {
+        this.controller_mock.expects("list_datasets").withExactArgs("10", undefined).once();
+
+        this.router.navigate("datasets/10", true);
+
+        this.controller_mock.verify();
+    });
+
+    it("should support dataset browser paging", function() {
+        this.controller_mock.expects("list_datasets").withExactArgs("10", "2").once();
+
+        this.router.navigate("datasets/10/2", true);
+
+        this.controller_mock.verify();
+    });
+
+    it("should show a single dataset", function() {
+        this.controller_mock.expects("edit_dataset").withExactArgs("17").once();
+
+        this.router.navigate("dataset/17", true);
+
+        this.controller_mock.verify();
+    });
+
+    it("should support per-dataset search", function() {
+        this.controller_mock.expects("search_dataset").withExactArgs("17", "query", undefined, undefined).once();
+
+        this.router.navigate("dataset/17/search/query", true);
+
+        this.controller_mock.verify();
+    });
+
+    it("should support per-dataset search limit", function() {
+        this.controller_mock.expects("search_dataset").withExactArgs("17", "query", "10", undefined).once();
+
+        this.router.navigate("dataset/17/search/query/10", true);
+
+        this.controller_mock.verify();
+    });
+
+    it("should support per-dataset search paging", function() {
+        this.controller_mock.expects("search_dataset").withExactArgs("17", "query", "10", "2").once();
+
+        this.router.navigate("dataset/17/search/query/10/2", true);
 
         this.controller_mock.verify();
     });
