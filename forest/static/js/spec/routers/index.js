d1937bfd7716b02711a6e8a52ad87cf4d532bde3
Christopher Groskopf <staringmonkey@gmail.com>
2011-11-09 15:17:00 -0600

    Added missing routing tests. Closes #126.

--- forest/static/js/spec/routers/index.js
+++ forest/static/js/spec/routers/index.js
@@ -1,5 +1,6 @@
 describe("Index Router", function() {
     beforeEach(function() {
+        // Fake out the Root view so its initialize won't fire
         FakeController = function() {}; 
         FakeController.prototype = PANDA.views.Root.prototype;
         this.fake_controller = new FakeController();
@@ -14,16 +15,34 @@ describe("Index Router", function() {
         this.router.navigate("away");
     });
 
-    it("should default to search", function() {
-        var route_spy = sinon.spy();
-        this.router.bind("route:search", route_spy);
+    it("should route to the login page", function() {
+        this.controller_mock.expects("login").once();
 
+        this.router.navigate("login", true);
+
+        this.controller_mock.verify();
+    });
+
+    it("should route to the logout page", function() {
+        this.controller_mock.expects("logout").once();
+
+        this.router.navigate("logout", true);
+
+        this.controller_mock.verify();
+    });
+
+    it("should route to the registration page", function() {
+        this.controller_mock.expects("register").once();
+
+        this.router.navigate("register", true);
+
+        this.controller_mock.verify();
+    });
+
+    it("should route to search", function() {
         this.controller_mock.expects("search").withExactArgs(undefined, undefined, undefined).once();
 
         this.router.navigate("", true);
-
-        expect(route_spy).toHaveBeenCalledOnce();
-        expect(route_spy).toHaveBeenCalledWith();
 
         this.controller_mock.verify();
     });
@@ -52,7 +71,7 @@ describe("Index Router", function() {
         this.controller_mock.verify();
     });
 
-    it("should support upload", function() {
+    it("should route to the upload page", function() {
         this.controller_mock.expects("upload").once();
 
         this.router.navigate("upload", true);
@@ -60,7 +79,7 @@ describe("Index Router", function() {
         this.controller_mock.verify();
     });
 
-    it("should support dataset browsing", function() {
+    it("should route to dataset browsing", function() {
         this.controller_mock.expects("list_datasets").withExactArgs(undefined, undefined).once();
 
         this.router.navigate("datasets", true);
@@ -84,7 +103,7 @@ describe("Index Router", function() {
         this.controller_mock.verify();
     });
 
-    it("should show a single dataset", function() {
+    it("should route to edit dataset", function() {
         this.controller_mock.expects("edit_dataset").withExactArgs("17").once();
 
         this.router.navigate("dataset/17", true);
@@ -92,7 +111,7 @@ describe("Index Router", function() {
         this.controller_mock.verify();
     });
 
-    it("should support per-dataset search", function() {
+    it("should route to per-dataset search", function() {
         this.controller_mock.expects("search_dataset").withExactArgs("17", "query", undefined, undefined).once();
 
         this.router.navigate("dataset/17/search/query", true);
