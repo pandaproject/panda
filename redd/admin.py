466556f7a7617d67db68eeaaa856423d60668926
Christopher Groskopf <staringmonkey@gmail.com>
2011-11-16 13:22:16 -0600

    Display categories on dataset view page.

--- redd/admin.py
+++ redd/admin.py
@@ -8,18 +8,26 @@ from tastypie.models import ApiAccess, ApiKey
 
 from redd.models import Category, Dataset, Notification, TaskStatus, Upload 
 
+# Register tastypie models
 admin.site.register(ApiKey)
 admin.site.register(ApiAccess)
 
-admin.site.register(Category)
-admin.site.register(Dataset)
-admin.site.register(Notification)
-admin.site.register(TaskStatus)
-admin.site.register(Upload)
-
+# Add API key reference to User admin
 class UserModelAdmin(UserAdmin):
     inlines = [ApiKeyInline]
 
 admin.site.unregister(User)
 admin.site.register(User, UserModelAdmin)
+
+# Custom Redd admins
+class DatasetAdmin(admin.ModelAdmin):
+    readonly_fields = ('schema', 'sample_data', 'dialect')
+
+admin.site.register(Category)
+admin.site.register(Dataset, DatasetAdmin)
+admin.site.register(Notification)
+admin.site.register(TaskStatus)
+admin.site.register(Upload)
+
+
 
