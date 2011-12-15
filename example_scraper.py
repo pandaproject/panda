a6f291a3beb7ecb7d67b81fe92e7cca6db2139dc
Christopher Groskopf <staringmonkey@gmail.com>
2011-12-15 12:42:46 -0600

    Update example scraper to use known slug.

--- example_scraper.py
+++ example_scraper.py
@@ -9,25 +9,28 @@ AUTH_PARAMS = {
     'email': 'panda@pandaproject.net',
     'api_key': 'edfe6c5ffd1be4d3bf22f69188ac6bc0fc04c84b'
 }
+DATASET_SLUG = 'test-dataset'
 
-# Create dataset
-dataset = {
-    'name': 'Test Dataset from API',
-    'schema': [{
-        'column': 'A',
-        'type': 'unicode'
-    }, {
-        'column': 'B',
-        'type': 'unicode'
-    }, {
-        'column': 'C',
-        'type': 'unicode'
-    }]
-}
+# Check if dataset exists
+response = requests.get(API + '/dataset/%s/' % DATASET_SLUG, params=AUTH_PARAMS)
 
-response = requests.post(API + '/dataset/', json.dumps(dataset), params=AUTH_PARAMS, headers={ 'Content-Type': 'application/json' })
+# Create dataset if necessary
+if response.status_code == 404:
+    dataset = {
+        'name': 'Test Dataset from API',
+        'schema': [{
+            'column': 'A',
+            'type': 'unicode'
+        }, {
+            'column': 'B',
+            'type': 'unicode'
+        }, {
+            'column': 'C',
+            'type': 'unicode'
+        }]
+    }
 
-dataset = json.loads(response.content)
+    response = requests.put(API + '/dataset/%s/' % DATASET_SLUG, json.dumps(dataset), params=AUTH_PARAMS, headers={ 'Content-Type': 'application/json' })
 
 # Write data
 data = { 'objects': [{
@@ -36,6 +39,5 @@ data = { 'objects': [{
     'data': ['More', 'data', 'here.']   
 }]}
 
-response = requests.put(API + '/dataset/%s/data/' % dataset['slug'], json.dumps(data), params=AUTH_PARAMS, headers={ 'Content-Type': 'application/json' })
+response = requests.put(API + '/dataset/%s/data/' % DATASET_SLUG, json.dumps(data), params=AUTH_PARAMS, headers={ 'Content-Type': 'application/json' })
 
-print response.content
