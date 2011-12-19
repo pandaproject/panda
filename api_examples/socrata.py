266a4ea25499238ab9bc7145e774b204b779b160
Christopher Groskopf <staringmonkey@gmail.com>
2011-12-19 16:55:16 -0600

    Cleaned up socrata example scraper.

--- api_examples/socrata.py
+++ api_examples/socrata.py
@@ -1,20 +1,34 @@
 #!/usr/bin/env python
 
+"""
+Example showing how to import data from the Socrata API.
+"""
+
 import json
 
 import requests
 
-API = 'http://localhost:8000/api/1.0'
-AUTH_PARAMS = {
+PANDA_API = 'http://localhost:8000/api/1.0'
+PANDA_AUTH_PARAMS = {
     'email': 'panda@pandaproject.net',
     'api_key': 'edfe6c5ffd1be4d3bf22f69188ac6bc0fc04c84b'
 }
-DATASET_SLUG = 'foia-request-log-311'
+PANDA_DATASET_SLUG = 'foia-request-log-311'
 
-DATA_URL = 'http://data.cityofchicago.org/api/views/j2p9-gdf5/rows.json'
+PANDA_DATASET_URL = '%s/dataset/%s/' % (PANDA_API, PANDA_DATASET_SLUG)
+PANDA_DATA_URL = '%s/dataset/%s/data/' % (PANDA_API, PANDA_DATASET_SLUG)
+
+SOCRATA_URL = 'http://data.cityofchicago.org/api/views/j2p9-gdf5/rows.json'
+
+# Utility functions
+def panda_get(url):
+    return requests.get(url, params=PANDA_AUTH_PARAMS)
+
+def panda_put(url, data):
+    return requests.post(url, data, params=PANDA_AUTH_PARAMS, headers={ 'Content-Type': 'application/json' })
 
 # Check if dataset exists
-response = requests.get(API + '/dataset/%s/' % DATASET_SLUG, params=AUTH_PARAMS)
+response = panda_get(PANDA_DATASET_URL)
 
 # Create dataset if necessary
 if response.status_code == 404:
@@ -39,13 +53,11 @@ if response.status_code == 404:
         }]
     }
 
-    response = requests.put(API + '/dataset/%s/' % DATASET_SLUG, json.dumps(dataset), params=AUTH_PARAMS, headers={ 'Content-Type': 'application/json' })
-
-def bulk_update(data):
-    requests.put(API + '/dataset/%s/data/' % DATASET_SLUG, json.dumps(data), params=AUTH_PARAMS, headers={ 'Content-Type': 'application/json' })
+    response = panda_put(PANDA_DATASET_URL, json.dumps(dataset))
 
 # Fetch latest data from Socrata
-response = requests.get(DATA_URL)
+print 'Fetching latest data'
+response = requests.get(SOCRATA_URL)
 
 data = json.loads(response.content)
 
@@ -63,10 +75,12 @@ for i, row in enumerate(data['data']):
     if i % 100 == 0:
         print 'Updating 100 rows...'
 
-        bulk_update(put_data)
+        panda_put(PANDA_DATA_URL, json.dumps(data))
         put_data['objects'] = []
         
 if put_data['objects']:
-    'Updating %i rows' % len(put_data['objects'])
-    bulk_update(put_data)
+    print 'Updating %i rows' % len(put_data['objects'])
+    panda_put(PANDA_DATA_URL, json.dumps(data))
+
+print 'Done'
 
