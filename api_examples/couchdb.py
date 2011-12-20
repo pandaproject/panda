198fb9996f84acd3ebdcb8fe39b693340dfb476a
Christopher Groskopf <staringmonkey@gmail.com>
2011-12-20 09:27:01 -0600

    Complete couchdb example scraper. Closes #265

--- api_examples/couchdb.py
+++ api_examples/couchdb.py
@@ -19,6 +19,7 @@ PANDA_DATASET_URL = '%s/dataset/%s/' % (PANDA_API, PANDA_DATASET_SLUG)
 PANDA_DATA_URL = '%s/dataset/%s/data/' % (PANDA_API, PANDA_DATASET_SLUG)
 PANDA_BULK_UPDATE_SIZE = 1000
 
+COUCHDB_ROOT_URL = 'http://datacouch.com/db/dc07acde3002cb1f62a08de546916097cd'
 COUCHDB_ROWS_URL = 'http://datacouch.com/db/dc07acde3002cb1f62a08de546916097cd/rows'
 COUCHDB_CHANGES_URL = 'http://datacouch.com/db/dc07acde3002cb1f62a08de546916097cd/_changes'
 
@@ -28,6 +29,23 @@ def panda_get(url):
 
 def panda_put(url, data):
     return requests.put(url, data, params=PANDA_AUTH_PARAMS, headers={ 'Content-Type': 'application/json' })
+
+def panda_delete(url):
+    return requests.delete(url, params=PANDA_AUTH_PARAMS, headers={ 'Content-Type': 'application/json' })
+
+def write_last_seq(last_seq):
+    with open('last_seq', 'w') as f:
+        f.write(str(last_seq))
+
+def read_last_seq():
+    with open('last_seq') as f:
+        return f.read().strip()
+
+def couchdb_row_to_panda_data(row):
+    return {
+        'data': [row['first_name'], row['last_name'], row['employer']],
+        'external_id': row['_id'] 
+    }
 
 # Check if dataset exists
 response = panda_get(PANDA_DATASET_URL)
@@ -51,6 +69,12 @@ if response.status_code == 404:
 
     response = panda_put(PANDA_DATASET_URL, json.dumps(dataset))
 
+    # Get changes that have come before so we can skip them in the future
+    response = requests.get(COUCHDB_CHANGES_URL)
+    data = json.loads(response.content)
+
+    write_last_seq(data['last_seq'])
+
     # Do a complete import of all data from CouchDB 
     response = requests.get(COUCHDB_ROWS_URL)
     data = json.loads(response.content)
@@ -60,10 +84,7 @@ if response.status_code == 404:
     }
 
     for i, row in enumerate(data['rows']):
-        put_data['objects'].append({
-            'data': [row['value']['first_name'], row['value']['last_name'], row['value']['employer']],
-            'external_id': row['value']['_id'] 
-        })
+        put_data['objects'].append(couchdb_row_to_panda_data(row['value']))
 
         if i and i % PANDA_BULK_UPDATE_SIZE == 0:
             print 'Updating %i rows...' % PANDA_BULK_UPDATE_SIZE
@@ -75,14 +96,54 @@ if response.status_code == 404:
         print 'Updating %i rows' % len(put_data['objects'])
         panda_put(PANDA_DATA_URL, json.dumps(put_data))
 
-    print 'Done'
+# Update existing dataset
+else:
+    # Where did we leave off?
+    last_seq = read_last_seq()
 
-# Fetch changes from CouchDB
-#print 'Fetching latest data'
-#response = requests.get(COUCHDB_CHANGES_URL)
+    response = requests.get(COUCHDB_CHANGES_URL, params={ 'since': last_seq })
+    data = json.loads(response.content)
+    
+    delete_ids = []
 
-#data = json.loads(response.content)
+    put_data = {
+        'objects': []
+    }
 
+    for i, row in enumerate(data['results']):
+        # Is this a deletion?
+        if row.get('deleted', False):
+            delete_ids.append(row['id'])
+            continue
 
+        doc_id = row['id']
 
+        detail_response = requests.get('%s/%s' % (COUCHDB_ROOT_URL, doc_id))
+        detail_data = json.loads(detail_response.content)
+
+        put_data['objects'].append(couchdb_row_to_panda_data(detail_data))
+
+        if i and i % PANDA_BULK_UPDATE_SIZE == 0:
+            print 'Updating %i rows...' % PANDA_BULK_UPDATE_SIZE
+
+            panda_put(PANDA_DATA_URL, json.dumps(put_data))
+            put_data['objects'] = []
+            
+    if put_data['objects']:
+        print 'Updating %i rows' % len(put_data['objects'])
+        panda_put(PANDA_DATA_URL, json.dumps(put_data))
+
+    # Process deletes
+    if delete_ids:
+        print 'Deleting %i rows' % len(delete_ids)
+
+        for deleted in delete_ids:
+            response = panda_delete('%s%s/' % (PANDA_DATA_URL, deleted))
+
+            print response
+
+    # Update location for next run
+    write_last_seq(data['last_seq'])
+
+print 'Done'
 
