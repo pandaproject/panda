3374c07d579c4314bcae73bc981e3ff36c2bdb72
Christopher Groskopf <staringmonkey@gmail.com>
2011-12-22 11:56:47 -0600

    Started to clean up admin.

--- redd/admin.py
+++ redd/admin.py
@@ -2,32 +2,31 @@
 
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import User
+from django.contrib.auth.models import Group, User
+from djcelery.models import CrontabSchedule, IntervalSchedule, PeriodicTask, TaskState, WorkerState
 from tastypie.admin import ApiKeyInline
-from tastypie.models import ApiAccess, ApiKey
+from tastypie.models import ApiKey
 
-from redd.models import Category, Dataset, Notification, TaskStatus, Upload 
+from redd.models import Category, TaskStatus 
+
+# Hide celery monitors
+admin.site.unregister(CrontabSchedule)
+admin.site.unregister(IntervalSchedule)
+admin.site.unregister(PeriodicTask)
+admin.site.unregister(TaskState)
+admin.site.unregister(WorkerState)
 
 # Register tastypie models
 admin.site.register(ApiKey)
-admin.site.register(ApiAccess)
 
 # Add API key reference to User admin
 class UserModelAdmin(UserAdmin):
     inlines = [ApiKeyInline]
 
+admin.site.unregister(Group)
 admin.site.unregister(User)
 admin.site.register(User, UserModelAdmin)
 
-# Custom Redd admins
-class DatasetAdmin(admin.ModelAdmin):
-    readonly_fields = ('data_upload', 'schema', 'row_count', 'sample_data', 'current_task', 'creation_date', 'creator', 'dialect')
-
 admin.site.register(Category)
-admin.site.register(Dataset, DatasetAdmin)
-admin.site.register(Notification)
 admin.site.register(TaskStatus)
-admin.site.register(Upload)
-
-
 
