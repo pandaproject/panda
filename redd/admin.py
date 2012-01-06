7357eaad61bc16374cb918533e3ed66768cfa2b8
Christopher Groskopf <staringmonkey@gmail.com>
2012-01-06 08:55:38 -0600

    Hide apikey inline in user add form.

--- redd/admin.py
+++ redd/admin.py
@@ -2,8 +2,13 @@
 
 from django import forms
 from django.contrib import admin
+from django.contrib.admin import helpers
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import Group, User
+from django.core.exceptions import PermissionDenied
+from django.db import models
+from django.utils.encoding import force_unicode
+from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext_lazy as _
 from djcelery.models import CrontabSchedule, IntervalSchedule, PeriodicTask, TaskState, WorkerState
 from tastypie.admin import ApiKeyInline
@@ -40,7 +45,6 @@ class PandaUserCreationForm(forms.ModelForm):
         user = super(PandaUserCreationForm, self).save(commit=False)
         # TODO
         #user.set_password(self.cleaned_data["password1"])
-        # TODO - generate API key
         # TODO - add to panda_user group
         if commit:
             user.save()
@@ -59,7 +63,73 @@ class UserModelAdmin(UserAdmin):
     )
 
     def add_view(self, request, form_url='', extra_context=None):
-        return super(UserModelAdmin, self).add_view(request, form_url, extra_context)
+        "The 'add' admin view for this model."
+        model = self.model
+        opts = model._meta
+
+        if not self.has_add_permission(request):
+            raise PermissionDenied
+
+        ModelForm = self.get_form(request)
+        formsets = []
+        if request.method == 'POST':
+            form = ModelForm(request.POST, request.FILES)
+            if form.is_valid():
+                new_object = self.save_form(request, form, change=False)
+                form_validated = True
+            else:
+                form_validated = False
+                new_object = self.model()
+
+            if form_validated:
+                self.save_model(request, new_object, form, change=False)
+                form.save_m2m()
+                for formset in formsets:
+                    self.save_formset(request, form, formset, change=False)
+
+                self.log_addition(request, new_object)
+                return self.response_add(request, new_object)
+        else:
+            # Prepare the dict of initial data from the request.
+            # We have to special-case M2Ms as a list of comma-separated PKs.
+            initial = dict(request.GET.items())
+            for k in initial:
+                try:
+                    f = opts.get_field(k)
+                except models.FieldDoesNotExist:
+                    continue
+                if isinstance(f, models.ManyToManyField):
+                    initial[k] = initial[k].split(",")
+            form = ModelForm(initial=initial)
+
+        adminForm = helpers.AdminForm(form, list(self.get_fieldsets(request)),
+            self.prepopulated_fields, self.get_readonly_fields(request),
+            model_admin=self)
+        media = self.media + adminForm.media
+
+        inline_admin_formsets = []
+        for inline, formset in zip(self.inline_instances, formsets):
+            fieldsets = list(inline.get_fieldsets(request))
+            readonly = list(inline.get_readonly_fields(request))
+            inline_admin_formset = helpers.InlineAdminFormSet(inline, formset,
+                fieldsets, readonly, model_admin=self)
+            inline_admin_formsets.append(inline_admin_formset)
+            media = media + inline_admin_formset.media
+
+        context = {
+            'title': _('Add %s') % force_unicode(opts.verbose_name),
+            'adminform': adminForm,
+            'is_popup': "_popup" in request.REQUEST,
+            'show_delete': False,
+            'media': mark_safe(media),
+            'inline_admin_formsets': inline_admin_formsets,
+            'errors': helpers.AdminErrorList(form, formsets),
+            'root_path': self.admin_site.root_path,
+            'app_label': opts.app_label,
+        }
+        context.update(extra_context or {})
+
+        return self.render_change_form(request, context, form_url=form_url, add=True)
 
 admin.site.unregister(Group)
 admin.site.unregister(User)
