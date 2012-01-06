e9e39d1a4dbdc8137c58f7287b0c6a5fbe896891
Christopher Groskopf <staringmonkey@gmail.com>
2012-01-06 17:00:12 -0600

    Ditch -e of git/hg installs.

--- config/settings.py
+++ config/settings.py
@@ -114,6 +114,7 @@ EMAIL_HOST = 'localhost'
 EMAIL_PORT = 1025
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
 DEFAULT_FROM_EMAIL = 'do.not.reply@pandaproject.net'
 
 # Django-compressor
