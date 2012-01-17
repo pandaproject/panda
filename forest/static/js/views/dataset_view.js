f0d4cd4a0ee1265e13b5146ba1f096f56bd3e9bb
Christopher Groskopf <staringmonkey@gmail.com>
2012-01-17 09:15:02 -0600

    Make display of related files consistent on dataset view page.

--- forest/static/js/views/dataset_view.js
+++ forest/static/js/views/dataset_view.js
@@ -1,5 +1,7 @@
 PANDA.views.DatasetView = Backbone.View.extend({
     template: PANDA.templates.dataset_view,
+    data_upload_template: PANDA.templates.data_upload_item,
+    related_upload_template: PANDA.templates.related_upload_item,
 
     initialize: function(options) {
         _.bindAll(this, "render");
@@ -10,7 +12,22 @@ PANDA.views.DatasetView = Backbone.View.extend({
     },
 
     render: function() {
-        this.el.html(this.template(this.dataset.results()));
+        data_uploads_html = this.dataset.data_uploads.map(_.bind(function(data_upload) {
+            return this.data_upload_template(data_upload.toJSON());
+        }, this));
+
+        related_uploads_html = this.dataset.related_uploads.map(_.bind(function(related_upload) {
+            return this.related_upload_template(related_upload.toJSON());
+        }, this));
+
+        context = {
+            'dataset': this.dataset.toJSON(true),
+            'categories': Redd.get_categories().toJSON(),
+            'data_uploads_html': data_uploads_html,
+            'related_uploads_html': related_uploads_html
+        }
+
+        this.el.html(this.template(context));
     }
 });
 
