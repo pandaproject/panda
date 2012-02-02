66b5116b44d02e27f1607024126095c4bd48591d
Christopher Groskopf <staringmonkey@gmail.com>
2012-02-02 12:03:49 -0600

    Style tweaks.

--- forest/static/js/application.js
+++ forest/static/js/application.js
@@ -33,3 +33,24 @@ PANDA.utils.make_context = function(ctx) {
     return ctx;
 }
 
+PANDA.utils.truncate = function(str, limit) {
+	var bits, i;
+
+	bits = str.split('');
+
+	if (bits.length > limit) {
+		for (i = bits.length - 1; i > -1; --i) {
+			if (i > limit) {
+				bits.length = i;
+			}
+			else if (' ' === bits[i]) {
+				bits.length = i;
+				break;
+			}
+		}
+
+		bits.push('...');
+	}
+
+	return bits.join('');
+}
