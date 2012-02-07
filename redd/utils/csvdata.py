23b8a881a2bc67413ea9ec6028733a768ff6f94f
Christopher Groskopf <staringmonkey@gmail.com>
2012-02-07 09:03:49 -0600

    Even better unicode error handling.

--- redd/utils/csvdata.py
+++ redd/utils/csvdata.py
@@ -6,7 +6,7 @@ from csvkit import CSVKitReader
 from csvkit.sniffer import sniff_dialect as csvkit_sniff
 from django.conf import settings
 
-from redd.exceptions import NotSniffableError
+from redd.exceptions import DataSamplingError, NotSniffableError
 
 def sniff_dialect(path):
     with open(path, 'r') as f:
@@ -34,12 +34,19 @@ def extract_column_names(path, dialect):
 def sample_data(path, dialect, sample_size):
     with open(path, 'r') as f:
         reader = CSVKitReader(f, **dialect)
-        reader.next() # skip headers
-            
-        samples = []
 
-        for row in islice(reader, sample_size):
-            samples.append(row)
+        try:
+            reader.next() # skip headers
+        except UnicodeDecodeError:
+            raise DataSamplingError('The header of this CSV file contains characters that are not UTF-8 encoded. PANDA supports only UTF-8 and UTF-8 compatible encodings for CSVs.')
+
+        try:  
+            samples = []
+
+            for row in islice(reader, sample_size):
+                samples.append(row)
+        except UnicodeDecodeError:
+            raise DataSamplingError('Row %i of this CSV file contains characters that are not UTF-8 encoded. PANDA supports only UTF-8 and UTF-8 compatible encodings for CSVs.' % (len(samples) + 1))
 
         return samples 
 
