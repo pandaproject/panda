4c4952a722216901dbf68ac71522f34f5b665936
Christopher Groskopf <staringmonkey@gmail.com>
2012-02-14 08:28:20 -0600

    Revert.

--- config/settings.py
+++ config/settings.py
@@ -115,7 +115,6 @@ AUTH_PROFILE_MODULE = 'panda.UserProfile'
 
 # Django-compressor
 COMPRESS_ENABLED = False 
-COMPRESS_JS_FILTERS = ['compressor.filters.jsmin.SlimItFilter']
 
 # Celery
 import djcelery
