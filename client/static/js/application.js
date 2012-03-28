2e20bfb13ebc34849a706a4271ad5da5f709492e
Christopher Groskopf <staringmonkey@gmail.com>
2012-03-28 13:20:28 -0500

    Fix us of return false.

--- client/static/js/application.js
+++ client/static/js/application.js
@@ -71,7 +71,7 @@ PANDA.utils.format_file_size = function(size) {
     return size.toFixed(1) + ' ' + units[i];
 }
 
-PANDA.utils.escapes_to_entities = function (text) {
-    return escape(text).replace(/%(..)/g,"&#x$1;");
+PANDA.utils.escapes_to_entities = function(text) {
+    return escaped_text.replace(/%(..)/g,"&#x$1;");
 };
 
