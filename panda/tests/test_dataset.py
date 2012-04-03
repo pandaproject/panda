538d75deb494c13e0b386e81c2722479e3120568
Christopher Groskopf <staringmonkey@gmail.com>
2012-04-03 10:22:27 -0500

    Fix unit test for min max date format.

--- panda/tests/test_dataset.py
+++ panda/tests/test_dataset.py
@@ -551,8 +551,8 @@ class TestDataset(TransactionTestCase):
         self.assertEqual([c['type'] for c in dataset.column_schema], ['unicode', 'date', 'int', 'bool', 'float', 'time', 'datetime', None, 'unicode'])
         self.assertEqual([c['indexed'] for c in dataset.column_schema], [True for c in upload.columns])
         self.assertEqual([c['indexed_name'] for c in dataset.column_schema], ['column_unicode_text', 'column_date_date', 'column_int_integer', 'column_bool_boolean', 'column_float_float', 'column_time_time', 'column_datetime_datetime', None, 'column_unicode_'])
-        self.assertEqual([c['min'] for c in dataset.column_schema], [None, u'1920-01-01 00:00:00', 40, None, 1.0, u'9999-12-31 00:00:00', u'1971-01-01 04:14:00', None, None])
-        self.assertEqual([c['max'] for c in dataset.column_schema], [None, u'1971-01-01 00:00:00', 164, None, 41800000.01, u'9999-12-31 14:57:13', u'2048-01-01 14:57:00', None, None])
+        self.assertEqual([c['min'] for c in dataset.column_schema], [None, u'1920-01-01T00:00:00', 40, None, 1.0, u'9999-12-31T00:00:00', u'1971-01-01T04:14:00', None, None])
+        self.assertEqual([c['max'] for c in dataset.column_schema], [None, u'1971-01-01T00:00:00', 164, None, 41800000.01, u'9999-12-31T14:57:13', u'2048-01-01T14:57:00', None, None])
         self.assertEqual(dataset.row_count, 5)
         self.assertEqual(dataset.locked, False)
 
