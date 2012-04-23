af4c6e03dfacc7d1f90ec29e53d3cae191642ffc
Christopher Groskopf <staringmonkey@gmail.com>
2012-04-23 13:45:32 -0500

    Make search chart an appropriate size.

--- client/static/js/views/dashboard.js
+++ client/static/js/views/dashboard.js
@@ -13,7 +13,24 @@ PANDA.views.Dashboard = Backbone.View.extend({
             dataType: "html",
             success: _.bind(function(response) {
                 $("#content").html(response);
-            })
+                this.draw_charts();
+            }, this)
+        });
+
+
+    },
+
+    draw_charts: function() {
+        var wrapper_width = $("#search-chart-wrapper").width();
+        $("#searches-by-day").width(wrapper_width).height(wrapper_width * 2 / 3);
+
+        $.plot($("#searches-by-day"), [window.DASHBOARD.search_data], {
+            series: {
+                bars: { show: true }
+            },
+            xaxis: {
+                ticks: window.DASHBOARD.search_ticks
+            }
         });
     }
 });
