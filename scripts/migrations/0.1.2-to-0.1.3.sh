099d3f3ccdb46e97d78f10bed6a753ec5fad688d
Christopher Groskopf <staringmonkey@gmail.com>
2012-04-24 16:13:36 -0500

    First whack at 0.1.3 upgrade script.

---
+++ scripts/migrations/0.1.2-to-0.1.3.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# PANDA Project migration script to ugprade version 0.1.2 to version 0.1.3.
+# Must be executed with sudo!
+
+set -x
+exec 1> >(tee /var/log/panda-upgrade-0.1.3.log) 2>&1
+
+echo "PANDA upgrade beginning."
+
+# Setup environment variables
+export DEPLOYMENT_TARGET="deployed"
+
+# Shutdown services
+service celeryd stop
+service nginx stop
+service uwsgi stop
+service solr stop
+
+# Install outstanding updates
+apt-get --yes update
+apt-get --yes upgrade
+
+# Fetch updated source code
+cd /opt/panda
+git pull
+git checkout 0.1.3
+
+# Update Python requirements
+pip install -U -r requirements.txt
+
+# Migrate database
+sudo -u panda -E python manage.py migrate panda
+
+# Regenerate assets
+sudo -u panda -E python manage.py collectstatic --noinput
+
+# Restart services
+service solr start 
+service uwsgi start
+service nginx start
+sudo service celeryd start
+
+echo "PANDA upgrade complete."
+
