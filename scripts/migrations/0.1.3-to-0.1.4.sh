38f1cf2770225a0184e66c1dec893995d75deeec
Christopher Groskopf <staringmonkey@gmail.com>
2012-05-09 08:49:47 -0500

    Add tentative migration script. #678.

---
+++ scripts/migrations/0.1.3-to-0.1.4.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# PANDA Project migration script to ugprade version 0.1.3 to version 0.1.4.
+# Must be executed with sudo!
+
+set -x
+exec 1> >(tee /var/log/panda-upgrade-0.1.4.log) 2>&1
+
+echo "PANDA upgrade beginning."
+
+# Setup environment variables
+export DEPLOYMENT_TARGET="deployed"
+
+# Shutdown services
+service celeryd stop
+service nginx stop
+service uwsgi stop
+service solr stop
+
+# Install outstanding updates
+apt-get --yes update
+apt-get --yes upgrade
+
+# Fetch updated source code
+cd /opt/panda
+git pull
+git checkout 0.1.4
+
+# Update Python requirements
+pip install -U -r requirements.txt
+
+# Migrate database
+sudo -u panda -E python manage.py migrate panda --noinput
+sudo -u panda -E python manage.py reindex_datasets
+
+# Regenerate assets
+sudo -u panda -E python manage.py collectstatic --noinput
+
+# Restart services
+service solr start 
+service uwsgi start
+service nginx start
+sudo service celeryd start
+
+echo "PANDA upgrade complete."
+
