e2da24d3c5cff8290e50586604ce84f4bf7fe767
Christopher Groskopf <staringmonkey@gmail.com>
2012-05-17 11:36:55 -0700

    Fix column filter search bug. Closes #694.

--- client/static/js/views/dataset_search.js
+++ client/static/js/views/dataset_search.js
@@ -234,14 +234,18 @@ PANDA.views.DatasetSearch = Backbone.View.extend({
                 }
             }
 
+            if (q) {
+                q += " AND ";
+            }
+
             if (v["operator"] == "is") {
-                q += " AND " + c["indexed_name"] + ':"' + v["value"] + '"';
+                q += c["indexed_name"] + ':"' + v["value"] + '"';
             } else if (v["operator"] == "is_greater") {
-                q += " AND " + c["indexed_name"] + ":[" + v["value"] + " TO *]";
+                q += c["indexed_name"] + ":[" + v["value"] + " TO *]";
             } else if (v["operator"] == "is_less") {
-                q += " AND " + c["indexed_name"] + ":[* TO " + v["value"] + "]";
+                q += c["indexed_name"] + ":[* TO " + v["value"] + "]";
             } else if (v["operator"] == "is_range") {
-                q += " AND " + c["indexed_name"] + ":[" + v["value"] + " TO " + v["range_value"] + "]";
+                q += c["indexed_name"] + ":[" + v["value"] + " TO " + v["range_value"] + "]";
             }
         }, this));
 
