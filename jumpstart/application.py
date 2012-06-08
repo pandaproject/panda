cae6c5b45e1668a3a4c0d4cd70d3524b4ddbb247
Christopher Groskopf <staringmonkey@gmail.com>
2012-06-08 15:32:44 -0500

    Testing.

--- jumpstart/application.py
+++ jumpstart/application.py
@@ -24,12 +24,19 @@ def index():
     if request.method == 'POST':
         timezone = request.form['timezone']
 
-        with open('test.txt', 'w') as f:
+        app.logger.debug('1')
+        with open(LOCAL_SETTINGS_PATH, 'w') as f:
             f.write("TIME_ZONE = '%s'\n" % timezone)
 
+        app.logger.debug('2')
         shutil.copy(UWSGI_CONF_SRC_PATH, UWSGI_CONF_DEST_PATH)
+        app.logger.debug('3')
         subprocess.call(['initctl', 'reload-configuration'])
+        app.logger.debug('4')
         subprocess.call(['service', 'uwsgi', 'restart'])
+        app.logger.debug('5')
+
+        return ''
     else:
         context = {
             'timezones': common_timezones
