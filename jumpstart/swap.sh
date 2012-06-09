a522305c7975ca97faedf2e6bc19e2a59226cca1
Christopher Groskopf <staringmonkey@gmail.com>
2012-06-08 21:01:37 -0500

    Testing.

--- jumpstart/swap.sh
+++ jumpstart/swap.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/bash
 
+echo "1" > /tmp/foo.txt
 cp /opt/panda/setup_panda/uwsgi.conf /etc/init/uwsgi.conf
+
+echo "2" >> /tmp/foo.txt
 service uwsgi stop
+echo "3" >> /tmp/foo.txt
 initctl reload-configuration
+echo "4" >> /tmp/foo.txt
 service uwsgi start
+echo "5" >> /tmp/foo.txt
