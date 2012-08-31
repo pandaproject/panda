b41bdf5b382f7afad95bc6d5bce542d65d936dca
Christopher Groskopf <staringmonkey@gmail.com>
2012-08-31 14:07:09 -0500

    More i18n.

---
+++ client/static/js/text/datasets_search.js
@@ -0,0 +1,8 @@
+PANDA.text.DatasetsSearch = function() {
+    return {
+        find_a_dataset: gettext("Find a dataset"),
+        all: gettext("all"),
+        query_placeholder: gettext("Enter a search query"),
+        search: gettext("Search")
+    }
+}
