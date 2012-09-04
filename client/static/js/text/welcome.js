d3eccb9bb29c874a3849648f2073ab156520e745
Christopher Groskopf <staringmonkey@gmail.com>
2012-09-04 10:14:46 -0500

    i18n: welcome.

---
+++ client/static/js/text/welcome.js
@@ -0,0 +1,22 @@
+PANDA.text.Welcome = function() {
+    return {
+        welcome_to_panda: gettext("Welcome to PANDA!"),
+        congratulations: gettext("Congratulations! If you're reading this then your organization has started using PANDA to manage its newsroom data. As a journalist you're bound to be skeptical of this new system. This brief introduction will introduce you to PANDA and explain how it can save you time, keep your data safe and improve the quality of your reporting."),
+        what_is_panda: gettext("What is PANDA?"),
+        a_data_library: gettext('First and foremost PANDA is a "data library", which means that it stores all the datasets you work with on a daily basis&mdash;voter registration records, police reports, water test results, etc. When you upload your data to PANDA it\'s stored safely away so that it can be easily found again, either by you or by another reporter in your organization.'),
+        a_search_engine: gettext("PANDA is also a search engine. By uploading your datasets to PANDA you make them searchable by everyone in your organization. This search feature is designed to work like Google, so you don't need to learn a new way of exploring the data."),
+        how_does_panda_help: gettext("How does PANDA help me?"),
+        panda_saves_time: gettext("PANDA will save you time"),
+        all_your_data: gettext("When all your data is stored in PANDA, it is just a quick search away. No more opening Excel and scrolling, scrolling, scrolling to find one piece of information. PANDA is fast and always available. Give data to your PANDA now and it will save you time on deadline."),
+        panda_is_safe: gettext("PANDA will keep your data safe"),
+        no_crashes: gettext("Never again will you have to worry about a crashing computer, a lost thumb drive or a scratched CD. Give data to PANDA and PANDA will keep it safe. With PANDA's search engine you will always be able to find it again."),
+        panda_improves_reporting: gettext("PANDA will improve your reporting"),
+        serendipity: gettext("PANDA encourages serendipity in the reporting process. By having access to all the newsworthy data in your newsroom you will uncover information you might have otherwise overlooked. For instance, a search for the name of a state senator might return a dataset of his political affiliations, a record of his graduation, a list of bills he has sponsored and that he has a brother who is an energy lobbyist."),
+        panda_helps_newsorgs: gettext("How does PANDA help my organization?"),
+        a_single_place: gettext("By providing a single place to store all the data reporters work with PANDA will encourage knowledge sharing, prevent catastrophic data loss and slow information attrition when reporters retire or change jobs. In particular we hope to eliminate the situation where more than one reporter is using FOIA or other channels to acquire the same dataset."),
+        get_started: gettext("Get started!"),
+        need_help: gettext("If you need additional help use the <strong>Help</strong> menu at the top of the screen."),
+        start_using_panda: gettext("Start using PANDA!"),
+        show_again: gettext("Show this page next time I log in")
+    }
+}
