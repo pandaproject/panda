236083ea96e96c3b04a42a5d3496a2705d6a64fe
Christopher Groskopf <staringmonkey@gmail.com>
2012-09-04 09:16:50 -0500

    More i18n

---
+++ client/static/js/text/inline_search_filters.js
@@ -0,0 +1,8 @@
+PANDA.text.DatasetSearchFilters = function() {
+    return {
+        add_to_search: gettext("Add to search"),
+        pick_a_column: gettext("Pick a column..."),
+        search: gettext("Search"),
+        remove: gettext("Remove")
+    }
+}
