fff96f2b1e7c6a61d1ef553d32257a97b89d7221
Christopher Groskopf <staringmonkey@gmail.com>
2012-09-04 10:45:52 -0500

    i18n: two modals.

---
+++ client/static/js/text/modal_change_password.js
@@ -0,0 +1,10 @@
+PANDA.text.ChangePassword = function() {
+    return {
+        change_password: gettext("Change password"),
+        current_password: gettext("Current password"),
+        new_password: gettext("New password"),
+        reenter_password: gettext("Reenter new password"),
+        cancel: gettext("Cancel"),
+        change: gettext("Change")
+    }
+}
---
+++ client/static/js/text/modal_dataset_edit.js
@@ -0,0 +1,11 @@
+PANDA.text.DatasetEdit = function() {
+    return {
+        edit: gettext("Edit"),
+        name: gettext("Name"),
+        description: gettext("Description"),
+        description_placeholder: gettext("Describe this dataset"),
+        categories: gettext("Categories"),
+        cancel: gettext("Cancel"),
+        save: gettext("Save")
+    }
+}
