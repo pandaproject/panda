c1d17cc16d3cb94514a368ee49d81599c416349c
Joe Germuska <joe@germuska.com>
2013-04-22 14:20:46 -0500

    s/adminstrator/administrator/g

--- client/static/js/text/login.js
+++ client/static/js/text/login.js
@@ -6,7 +6,7 @@ PANDA.text.Login = function() {
         forgotten_password: gettext("Forget your password?"),
         login: gettext("Login"),
         enter_your_email: gettext("Enter your email address below and PANDA will send you a link you can follow to reset your password."),
-        contact_admin: gettext("Your PANDA adminstrator has not yet configured email for your server, so your password can not be automatically reset. Please contact your adminstrator and ask them to either configure email or to reset your password manually."),
+        contact_admin: gettext("Your PANDA administrator has not yet configured email for your server, so your password can not be automatically reset. Please contact your administrator and ask them to either configure email or to reset your password manually."),
         cancel: gettext("Cancel"),
         send_email: gettext("Send email")
     }
