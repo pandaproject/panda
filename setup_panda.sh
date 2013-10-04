716c011773de6d158416427bbcb2a90669b4aa88
Christopher Groskopf <staringmonkey@gmail.com>
2013-10-04 13:08:25 -0700

    Rev to 1.1.0. Closes #893.

--- setup_panda.sh
+++ setup_panda.sh
@@ -8,7 +8,7 @@ exec 1> >(tee /var/log/panda-install.log) 2>&1
 
 echo "PANDA installation beginning."
 
-VERSION="1.0.3"
+VERSION="1.1.0"
 CONFIG_PATH="/opt/panda/setup_panda"
 
 # Setup environment variables
