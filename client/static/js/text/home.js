4d17ed23278825f637c6dce1db1ad902f8190c2f
Christopher Groskopf <staringmonkey@gmail.com>
2013-10-05 11:44:38 -0700

    Move dashboard link to header. Spec python recs. Closes #903.

--- client/static/js/text/home.js
+++ client/static/js/text/home.js
@@ -2,7 +2,6 @@ PANDA.text.Home = function() {
     return {
         recently_added_datasets: gettext("Recently added datasets"),
         created_timestamp: gettext("Created %(timeago)s"),
-        browse_all_datasets: gettext("Browse all %(count)s datasets"),
-        dashboard_link: gettext("Want to see how your PANDA is being used? Visit the <a %(link)s>Dashboard</a>!")
+        browse_all_datasets: gettext("Browse all %(count)s datasets")
     }
 }
