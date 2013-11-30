17cb1a2027580e91cbcf7b0d34975ed023600259
Christopher Groskopf <staringmonkey@gmail.com>
2013-11-30 10:59:57 -0600

    Fix admin login form enforcing 30 char username limit. Closes #931.

--- config/urls.py
+++ config/urls.py
@@ -4,30 +4,32 @@ from django.conf import settings
 from django.conf.urls.defaults import include, patterns
 from django.contrib import admin
 
+from longerusername.forms import AuthenticationForm
+
 # Jumpstart mode
 if settings.SETTINGS == 'jumpstart':
     urlpatterns = patterns('',
         (r'', include('jumpstart.urls')),
-        (r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
-            { 'document_root': settings.STATIC_ROOT,
-                'show_indexes': True }),
+        (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.STATIC_ROOT,
+            'show_indexes': True
+        }),
     )
 # Normal mode
 else:
     admin.autodiscover()
     admin.site.index_template = 'admin/panda_index.html'
+    admin.site.login_form = AuthenticationForm
 
     urlpatterns = patterns('',
         (r'', include('panda.urls')),
         (r'', include('client.urls')),
-        (r'^admin/login/$', 'django.contrib.auth.views.login', {'template_name': 'admin/login.html'}),
-        (r'^admin/logout/$', 'django.contrib.auth.views.logout'),
-        
         (r'^admin/settings/', include('livesettings.urls')),
         (r'^admin/', include(admin.site.urls)),
 
-        # Should never be used in production, as nginx will server these paths
-        (r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
-            { 'document_root': settings.STATIC_ROOT,
-                'show_indexes': True }),
+        # Should never be used in production, as nginx will serve these paths
+        (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.STATIC_ROOT,
+            'show_indexes': True
+        }),
     )
